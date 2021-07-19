a06751318283aac15f3b5a941681ad4f8b05726f
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-19 16:26:56 -0300

    chore: Generated the Spring Boot project w/ Maven

---
+++ src/main/java/com/digital/crud/meetingroom/meetingroom/MeetingroomApplication.java
@@ -0,0 +1,13 @@
+package com.digital.crud.meetingroom.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/crud/meetingroom/meetingroom/MeetingroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.crud.meetingroom.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
